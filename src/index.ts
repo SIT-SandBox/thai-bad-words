d8cb35293f60497e638241e8b2326083ec33b91c
9hodev <siaskkh4884@gmail.com>
2025-03-15 14:31:21 +0700

    rm case sensitive and minor change

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,13 @@ function checkBadWords(input: string): void {
     }
 
     if (trie.search(cleanedInput)) {
-        throw new Error(`Bad words detected! Found: ${cleanedInput}`);
+        throw new Error(`Bad words detected! Found: ${trie.search(cleanedInput)}`);
     }
 }
 
 export async function scanBadWords(input: Record<string,any> | string):Promise<void> {
     if (typeof input === 'string'){
-        checkBadWords(input);
+        checkBadWords(input.toLowerCase());
     }else{
         for (const key in input) {
             if (input.hasOwnProperty(key)) {
@@ -35,7 +35,7 @@ export async function scanBadWords(input: Record<string,any> | string):Promise<v
                     if (Array.isArray(value)) {
                         for (const item of value) {
                             if (typeof item === 'string') {
-                                checkBadWords(item);
+                                checkBadWords(item.toLowerCase());
                             } else if (typeof item === 'object' && item !== null) {
                                 await scanBadWords(item);
                             }
@@ -44,7 +44,7 @@ export async function scanBadWords(input: Record<string,any> | string):Promise<v
                         await scanBadWords(value);
                     }
                 } else if (typeof value === 'string') {
-                    checkBadWords(value);
+                    checkBadWords(value.toLowerCase());
                 }
             }
         }
@@ -52,28 +52,32 @@ export async function scanBadWords(input: Record<string,any> | string):Promise<v
 }
 
 export function addBadWords(newBadWords: string[]): void {
-    rootWords.push(...newBadWords);
+    const lowerCaseWords = newBadWords.map(word => word.toLowerCase());
+    rootWords.push(...lowerCaseWords);
     generateBadWords();
-    newBadWords.forEach(word => trie.insert(word));
+    lowerCaseWords.forEach(word => trie.insert(word));
 }
 
 export function removeBadWords(wordsToRemove: string[]): void {
-    const updatedRootWords = rootWords.filter(word => !wordsToRemove.includes(word));
+    const lowerCaseWordsToRemove = wordsToRemove.map(word => word.toLowerCase());
+    const updatedRootWords = rootWords.filter(word => !lowerCaseWordsToRemove.includes(word.toLowerCase()));
     rootWords.length = 0;
     rootWords.push(...updatedRootWords);
     generateBadWords();
     const newTrie = new Trie();
-    badWordsList.forEach(word => newTrie.insert(word));
+    badWordsList.forEach(word => newTrie.insert(word.toLowerCase()));
     trie.root = newTrie.root;
 }
 
 export function addPrefixes(newPrefixes: string[]): void {
-    prefixes.push(...newPrefixes);
+    const lowerCasePrefixes = newPrefixes.map(prefix => prefix.toLowerCase());
+    prefixes.push(...lowerCasePrefixes);
     generateBadWords();
 }
 
 export function addIgnoreList(newIgnoreWords: string[]): void {
-    ignoreList.push(...newIgnoreWords);
+    const lowerCaseIgnoreWords = newIgnoreWords.map(word => word.toLowerCase());
+    ignoreList.push(...lowerCaseIgnoreWords);
 }
 
 export function getBadWords(): string[] {
